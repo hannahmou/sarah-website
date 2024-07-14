b2c736c8bca0d1a684f86d5c6348e73804cb671c
Hannah Mou <hannahmou05@hotmail.com>
2024-07-14 01:43:44 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="min-h-screen grid place-content-center">
+      <div className="bg-emerald-500 w-52 h-52 grid place-content-center">
+        <div className="bg-teal-200 w-32 h-32 rounded-full grid place-content-center">
+          <div className="bg-red-500 w-16 h-16 rounded-full"></div>
+        </div>
+      </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
