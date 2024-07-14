0f73bd645dd9e88c943beda09ff698e19299ba06
Hannah Mou <hannahmou05@hotmail.com>
2024-07-14 01:44:34 -0700

    Adding Hello to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
       <div className="bg-emerald-500 w-52 h-52 grid place-content-center">
         <div className="bg-teal-200 w-32 h-32 rounded-full grid place-content-center">
           <div className="bg-red-500 w-16 h-16 rounded-full"></div>
+          <h1>Hello</h1>
         </div>
       </div>
       </div>
